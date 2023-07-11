12bd367551c69172d23910722378de52eb673ae0
Lucas <mazalucas@hotmail.com>
2023-07-11 12:49:49 -0300

    feat(*): ported to v2.2, disable chat ending

--- plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
+++ plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Actions } from '@twilio/flex-ui'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Actions } from "@twilio/flex-ui";
 
-import { PauseIcon } from '@twilio-paste/icons/esm/PauseIcon'
-import { Spinner } from '@twilio-paste/core/spinner'
+import { Button } from "@twilio-paste/core";
+import { Spinner } from "@twilio-paste/core/spinner";
 
 const IconWrapper = styled.div`
-  margin: 0.8rem;
-  cursor: ${props => (props.isLoading ? 'not-allowed' : 'pointer')};
-`
+  margin: auto;
+  cursor: ${(props) => (props.isLoading ? "not-allowed" : "pointer")};
+`;
 
-export const ParkButton = props => {
-  const [isLoading, setIsLoading] = useState(false)
+export const ParkButton = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
       {isLoading ? (
         <IconWrapper isLoading={isLoading}>
-          <Spinner size='sizeIcon40' decorative={false} title='Loading' />
+          <Spinner size="sizeIcon40" decorative={false} title="Loading" />
         </IconWrapper>
       ) : (
-        <IconWrapper
-          onClick={() => {
-            setIsLoading(true)
-            Actions.invokeAction('ParkInteraction', { task: props.task })
-          }}
-        >
-          <PauseIcon
-            decorative={false}
-            title='Pause Interaction'
-            size='sizeIcon40'
-          />
+        <IconWrapper>
+          <Button
+            variant="destructive_secondary"
+            onClick={() => {
+              setIsLoading(true);
+              Actions.invokeAction("ParkInteraction", { task: props.task });
+            }}
+          >
+            Park Chat
+          </Button>
         </IconWrapper>
       )}
     </>
-  )
-}
+  );
+};
