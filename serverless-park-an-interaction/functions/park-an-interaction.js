6e4a50a0534d43b374f11ac4f97f3bf5750e0ff8
Lucas <mazalucas@hotmail.com>
2023-12-11 14:22:20 -0300

    feat(*): ignore parking logic

--- serverless-park-an-interaction/functions/park-an-interaction.js
+++ serverless-park-an-interaction/functions/park-an-interaction.js
@@ -1,20 +1,21 @@
-const path = Runtime.getFunctions()['response-header'].path
-const response = require(path).response()
+const path = Runtime.getFunctions()["response-header"].path;
+const response = require(path).response();
+const axios = require("axios");
 
 exports.handler = async function (context, event, callback) {
-  const client = context.getTwilioClient()
-  const CONVERSATIONS_WEBHOOK_URL = context.CONVERSATIONS_WEBHOOK_URL
+  const client = context.getTwilioClient();
+  const CONVERSATIONS_WEBHOOK_URL = context.CONVERSATIONS_WEBHOOK_URL;
 
-  const interactionSid = event.interactionSid
-  const channelSid = event.channelSid
-  const participantSid = event.participantSid
-  const conversationSid = event.conversationSid
-  const taskSid = event.taskSid
-  const workflowSid = event.workflowSid
-  const taskChannelUniqueName = event.taskChannelUniqueName
-  const targetSid = event.targetSid
-  const workerName = event.workerName
-  const taskAttributes = event.taskAttributes
+  const interactionSid = event.interactionSid;
+  const channelSid = event.channelSid;
+  const participantSid = event.participantSid;
+  const conversationSid = event.conversationSid;
+  const taskSid = event.taskSid;
+  const workflowSid = event.workflowSid;
+  const taskChannelUniqueName = event.taskChannelUniqueName;
+  const targetSid = event.targetSid;
+  const workerName = event.workerName;
+  const taskAttributes = event.taskAttributes;
 
   try {
     // Remove the agent
@@ -22,47 +23,52 @@ exports.handler = async function (context, event, callback) {
       .interaction(interactionSid)
       .channels(channelSid)
       .participants(participantSid)
-      .update({ status: 'closed' })
-      .then(interaction_channel_participant =>
+      .update({ status: "closed" })
+      .then((interaction_channel_participant) =>
         console.log(interaction_channel_participant)
-      )
+      );
 
-    // Create the webhook and update conversation attributes
+    // Create the webhook and update conversation attributes (if not ignoring parking logic)
+    let webhook = null;
+    if (!taskAttributes?.ignoreParkingLogic)
+      webhook = await client.conversations
+        .conversations(conversationSid)
+        .webhooks.create({
+          "configuration.method": "POST",
+          "configuration.filters": ["onMessageAdded"],
+          "configuration.url": CONVERSATIONS_WEBHOOK_URL,
+          target: "webhook",
+        });
+
+    const webhookSid = webhook?.sid;
+    const attributes = {
+      // Detail if the parking logic webhook was ignored or not.
+      interactionSid,
+      channelSid,
+      participantSid,
+      taskSid,
+      workflowSid,
+      taskChannelUniqueName,
+      targetSid,
+      workerName,
+      taskAttributes,
+      webhookSid,
+    };
+
+    // Set the parked flag.
+    if (taskAttributes?.ignoreParkingLogic) attributes['hasBeenParked'] = true;
+
     await client.conversations
       .conversations(conversationSid)
-      .webhooks.create({
-        'configuration.method': 'POST',
-        'configuration.filters': ['onMessageAdded'],
-        'configuration.url': CONVERSATIONS_WEBHOOK_URL,
-        target: 'webhook'
-      })
-      .then(async webhook => {
-        const webhookSid = webhook.sid
-        const attributes = {
-          interactionSid,
-          channelSid,
-          participantSid,
-          taskSid,
-          workflowSid,
-          taskChannelUniqueName,
-          targetSid,
-          workerName,
-          taskAttributes,
-          webhookSid
-        }
+      .update({ attributes: `${JSON.stringify(attributes)}` })
+      .then((conversation) => {
+        console.log("conversation attributes updated");
+        console.log(conversation);
+      });
 
-        await client.conversations
-          .conversations(conversationSid)
-          .update({ attributes: `${JSON.stringify(attributes)}` })
-          .then(conversation => {
-            console.log('conversation attributes updated')
-            console.log(conversation)
-          })
-      })
-
-    callback(null, response)
+    callback(null, response);
   } catch (error) {
-    console.log(error)
-    callback(error)
+    console.log(error);
+    callback(error);
   }
-}
+};
