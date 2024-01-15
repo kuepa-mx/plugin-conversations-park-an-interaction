0f6ce29fefc832e1bd8212848ca100a9b5eb5e70
Lucas <mazalucas@hotmail.com>
2024-01-15 14:13:12 -0300

    fix(*): better remove webhooks

--- serverless-park-an-interaction/functions/unpark-an-interaction.protected.js
+++ serverless-park-an-interaction/functions/unpark-an-interaction.protected.js
@@ -1,41 +1,73 @@
-const path = Runtime.getFunctions()['response-header'].path
-const response = require(path).response()
+const path = Runtime.getFunctions()["response-header"].path;
+const response = require(path).response();
+
+async function removeAllWebhooks(client, conversationSid) {
+  try {
+    // Remove all scoped-webhooks to avoid triggering the webhook again
+    const webhooks = await client.conversations
+      .conversations(conversationSid)
+      .webhooks.list();
+
+    if (webhooks?.length) {
+      for (const webhook of webhooks) await webhook.remove();
+    }
+  } catch (e) {
+    console.error(`Failed to remove webhooks from ${conversationSid}: ${e}`);
+  }
+}
 
 exports.handler = async function (context, event, callback) {
-  const client = context.getTwilioClient()
-  const workspaceSid = context.WORKSPACE_SID
+  const client = context.getTwilioClient();
+  const workspaceSid = context.WORKSPACE_SID;
 
-  const conversationSid = event.ConversationSid
+  const conversationSid = event.ConversationSid;
 
   try {
     // Fetch the conversation attributes updated when parked
+    const conversation = await client.conversations
+      .conversations(conversationSid)
+      .fetch();
+
+    // Parse attributes
+    const { attributes } = conversation;
+
     const {
       interactionSid,
       channelSid,
       taskAttributes,
       taskChannelUniqueName,
       webhookSid,
-      workflowSid
-    } = await client.conversations
-      .conversations(conversationSid)
-      .fetch()
-      .then(convo => {
-        const attributes = JSON.parse(convo.attributes)
-        return {
-          interactionSid: attributes.interactionSid,
-          channelSid: attributes.channelSid,
-          taskAttributes: attributes.taskAttributes,
-          taskChannelUniqueName: attributes.taskChannelUniqueName,
-          webhookSid: attributes.webhookSid,
-          workflowSid: attributes.workflowSid
-        }
-      })
+      workflowSid,
+    } = JSON.parse(attributes);
+
+    /* // Obtain participants to check for the 7900 number
+    // TODO: REMOVE THIS AFTER SOME TIME.
+    const participants = await conversation.participants().list();
+
+    if (participants?.length) {
+      // Check if the 7900 number is in the conversation
+      const has7900 = participants.find(
+        (participant) =>
+          participant?.messagingBinding?.proxy_address ===
+          "whatsapp:+15512927900"
+      );
+
+      if (has7900?.length) {
+        // Remove all scoped-webhooks to not send it back to Flex.
+        await removeAllWebhooks(client, conversationSid);
+
+        // End the function execution.
+        callback(null, response);
+        return;
+      }
+    } */
 
     // Remove webhook so it doesn't keep triggering if parked more than once
+    // await removeAllWebhooks(client, conversationSid);
     await client.conversations
       .conversations(conversationSid)
       .webhooks(webhookSid)
-      .remove()
+      .remove();
 
     // Create a new task through the invites endpoint. Alternatively you can pass
     // a queue_sid and a worker_sid inside properties to add a specific agent back to the interation
@@ -48,14 +80,14 @@ exports.handler = async function (context, event, callback) {
             workspace_sid: workspaceSid,
             workflow_sid: workflowSid,
             task_channel_unique_name: taskChannelUniqueName,
-            attributes: taskAttributes
-          }
-        }
-      })
+            attributes: taskAttributes,
+          },
+        },
+      });
 
-    callback(null, response)
+    callback(null, response);
   } catch (error) {
-    console.log(error)
-    callback(error)
+    console.log(error);
+    callback(error);
   }
-}
+};
